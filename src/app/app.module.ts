bf073c36a4e8de7265a01ecde2a6ce54d7420156
ProfZidane <madazada0@gmail.com>
2025-10-24 14:24:28 +0200

    add modal view detail on recommandation home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { EditTripComponent } from './pages/edit-trip/edit-trip.component';
 import { ViewTripComponent } from './pages/view-trip/view-trip.component';
 import { CreateTripComponent } from './pages/create-trip/create-trip.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { RecoTripDetailModalComponent } from './shared/modals/reco-trip-detail-modal/reco-trip-detail-modal.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { CreateTripComponent } from './pages/create-trip/create-trip.component';
     HomeComponent,
     EditTripComponent,
     ViewTripComponent,
-    CreateTripComponent
+    CreateTripComponent,
+    NavbarComponent,
+    FooterComponent,
+    RecoTripDetailModalComponent
   ],
   imports: [
     BrowserModule,
